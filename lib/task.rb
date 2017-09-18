ba959d373aeb8953426cc0840cbe9f01ebfe61eb
Karla Buckner and Riki Montgomery <karla@>
2017-09-18 14:29:17 -0700

    sort by due date and pass all specs

--- lib/task.rb
+++ lib/task.rb
@@ -1,30 +1,31 @@
 class Task
-  attr_reader(:description, :list_id)
+  attr_reader(:description, :due_date, :list_id)
 
   def initialize(attributes)
     @description = attributes.fetch(:description)
+    @due_date = attributes.fetch(:due_date)
     @list_id = attributes.fetch(:list_id)
   end
 
   def self.all
-    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    returned_tasks = DB.exec("SELECT * FROM tasks ORDER BY due_date")
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch("description")
+      due_date = task.fetch("due_date")
 # The information comes out of the database as a string.
       list_id = task.fetch("list_id").to_i()
-      tasks.push(Task.new({:description => description, :list_id => list_id}))
+      tasks.push(Task.new({:description => description, :due_date => due_date, :list_id => list_id}))
     end
     tasks
   end
 
-# ////////
   def save
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+    DB.exec("INSERT INTO tasks (description, due_date, list_id) VALUES ('#{@description}', '#{@due_date}', #{@list_id});")
   end
-# ////////
 
   def ==(another_task)
-    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
+    self.description().==(another_task.description()).&(self.due_date().==(another_task.due_date())).&(self.list_id().==(another_task.list_id()))
   end
+
 end
